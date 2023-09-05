fa5cd041f1657eef3940bf7bdcf6eb0b00adfa5a
davebeerensdesigns <84903154+davebeerensdesigns@users.noreply.github.com>
2023-09-05 09:55:36 +0200

    solution

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        int i = 5;
-        int odd = 0;
-        int even = 0;
-        while (i <= 20){
-            if(even == 5){
+        int startNumber = 4;
+        int endNumber = 20;
+        int oddCount = 0;
+        int evenCount = 0;
+        while (startNumber <= endNumber){
+            startNumber++;
+            if(!isEvenNumber(startNumber)){
+                oddCount++;
+                continue;
+            }
+            System.out.println("Even number " + startNumber);
+            evenCount++;
+            if(evenCount >= 5){
                 break;
             }
-            if(isEvenNumber(i)){
-                System.out.println(i);
-                even += 1;
-            } else {
-                odd += 1;
-            }
-            i++;
         }
-        System.out.println("total odds = " + odd);
-        System.out.println("total evens = " + even);
+        System.out.println("total odd numbers found = " + oddCount);
+        System.out.println("total even numbers found = " + evenCount);
     }
     public static boolean isEvenNumber(int number){
         return (number % 2 == 0);
